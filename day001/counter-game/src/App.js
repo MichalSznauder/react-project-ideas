162c6fce8ff2c0a94ce0f58fe469cc410f3288d1
MichalSznauder <michal.sznauder@gmail.com>
2025-03-11 17:29:07 +0100

    optimized component

--- day001/counter-game/src/App.js
+++ day001/counter-game/src/App.js
@@ -5,3 +5,27 @@ function App() {
 }
 
 export default App;
+import React, { useCallback, useMemo } from 'react';
+
+const HomePage = ({ data, onAction }) => {
+  // Przykład użycia useMemo do zapamiętania kosztownych obliczeń
+  const computedData = useMemo(() => {
+    return data.map(item => item.value * 2);
+  }, [data]);
+
+  // Przykład użycia useCallback do zapamiętania funkcji
+  const handleAction = useCallback(() => {
+    onAction(computedData);
+  }, [onAction, computedData]);
+
+  return (
+    <div>
+      {computedData.map((value, index) => (
+        <div key={index}>{value}</div>
+      ))}
+      <button onClick={handleAction}>Perform Action</button>
+    </div>
+  );
+};
+
+export default React.memo(HomePage);
